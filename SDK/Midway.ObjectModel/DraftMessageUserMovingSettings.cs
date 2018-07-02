d5ccda883337b48494309085737f0e9099cc0f93
Леонтьев Евгений (Leontiev_EV) <Leontiev_EV@synerdocs.ru>
2018-07-02 10:17:01 +0400

    Версия 2.23.3

---
+++ SDK/Midway.ObjectModel/DraftMessageUserMovingSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Midway.ObjectModel
+{
+    /// <summary>
+    /// Настройки передачи черновика сообщения по ящику организации и логину пользователя.
+    /// </summary>
+    public class DraftMessageUserMovingSettings : DraftMessageMovingSettings
+    {
+        /// <summary>
+        /// Ящик организации, которой будет передан черновик.
+        /// </summary>
+        [DataMember]
+        public string BoxAddress { get; set; }
+
+        /// <summary>
+        /// Логин пользователя, которому будет передан черновик.
+        /// </summary>
+        [DataMember(IsRequired = true)]
+        public string Login { get; set; }
+    }
+}
