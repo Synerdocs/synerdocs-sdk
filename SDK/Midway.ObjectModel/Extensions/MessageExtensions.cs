50b16a598333dd5ccb47c0828411fb780c79d72c
Холмогоров Виктор (Kholmogorov_VA) <Kholmogorov_VA@directum.ru>
2018-12-07 15:57:57 +0400

    Версия 2.27.0

--- SDK/Midway.ObjectModel/Extensions/MessageExtensions.cs
+++ SDK/Midway.ObjectModel/Extensions/MessageExtensions.cs
@@ -7,60 +7,60 @@ namespace Midway.ObjectModel.Extensions
     public static class MessageExtensions
     {
         /// <summary>
-        /// Формирует список получателей ответного сообщения
+        /// Формирует список получателей ответного сообщения.
         /// </summary>
-        /// <param name="message">оригинальное сообщение</param>
-        /// <param name="sender">отправитель ответного сообщения</param>
-        /// <returns></returns>
+        /// <param name="message">Оригинальное сообщение.</param>
+        /// <param name="sender">Отправитель ответного сообщения.</param>
+        /// <returns>Получатели сообщения.</returns>
         public static MessageRecipient[] GetRecipientListForSender(this Message message, MessageRecipient sender)
         {
+            var isOriginalSenderEqualsCurrent = message.From.Equals(sender.OrganizationBoxId, StringComparison.InvariantCultureIgnoreCase);
+
+            // Добавляем отправителя оригинального сообщения.
             var recipients = new List<MessageRecipient>();
+            if (!isOriginalSenderEqualsCurrent)
+                recipients.Add(new MessageRecipient
+                {
+                    OrganizationBoxId = message.From,
+                    DepartmentId = message.FromDepartment
+                });
 
-            // добавляем отправителя оригинального сообщения
-            var senderAsRecipient =
-                new MessageRecipient
-                    {
-                        OrganizationBoxId = message.From,
-                        DepartmentId = message.FromDepartment
-                    };
-            if (!sender.Equals(senderAsRecipient))
-                recipients.Add(senderAsRecipient);
-
-            // добавляем всех получателей, не включая указанного
+            // Добавляем всех получателей, не включая указанного.
             if (sender != null)
             {
-                recipients.AddRange(
-                    message.Recipients
-                        .Where(r => !r.Equals(sender))
-                        .ToList());
+                recipients.AddRange(isOriginalSenderEqualsCurrent
+                    ? message.Recipients
+                    : message.Recipients.Where(r => !r.Equals(sender)));
             }
 
             return recipients.ToArray();
         }
 
         /// <summary>
-        /// Формирует список получателей ответного сообщения
+        /// Формирует список получателей ответного сообщения.
         /// </summary>
-        /// <param name="message">оригинальное сообщение</param>
-        /// <param name="senderBox">ящик отправителя ответного сообщения</param>
-        /// <returns></returns>
+        /// <param name="message">Оригинальное сообщение.</param>
+        /// <param name="senderBox">Ящик отправителя ответного сообщения.</param>
+        /// <returns>Получатели сообщения.</returns>
         public static MessageRecipient[] GetRecipientListForSender(this Message message, string senderBox = null)
         {
+            var isOriginalSenderEqualsCurrent = message.From.Equals(senderBox, StringComparison.InvariantCultureIgnoreCase);
+
+            // Добавляем отправителя оригинального сообщения.
             var recipients = new List<MessageRecipient>();
-
-            // добавляем отправителя оригинального сообщения
-            if (!message.From.Equals(senderBox, StringComparison.InvariantCultureIgnoreCase))
+            if (!isOriginalSenderEqualsCurrent)
                 recipients.Add(new MessageRecipient
-                    {
-                        OrganizationBoxId = message.From,
-                        DepartmentId = message.FromDepartment
-                    });
+                {
+                    OrganizationBoxId = message.From,
+                    DepartmentId = message.FromDepartment
+                });
 
-            // добавляем всех получателей, не включая указанного
-            recipients.AddRange(
-                message.Recipients.Where(r => !r.OrganizationBoxId.Equals(senderBox, StringComparison.InvariantCultureIgnoreCase)));
+            // Добавили всех получателей и выбрали среди них различных.
+            recipients.AddRange(isOriginalSenderEqualsCurrent
+                    ? message.Recipients
+                    : message.Recipients.Where(r => !r.OrganizationBoxId.Equals(senderBox, StringComparison.InvariantCultureIgnoreCase)));
 
-            return recipients.ToArray();
+            return recipients.Distinct().ToArray();
         }
     }
 }
