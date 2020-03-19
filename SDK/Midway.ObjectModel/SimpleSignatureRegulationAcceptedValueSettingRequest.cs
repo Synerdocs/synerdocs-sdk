818642f71bb8ae431daf8542875b87692806657f
Чувашов Алексей (Chuvashov_AM) <Chuvashov_AM@directum.ru>
2020-03-19 11:14:04 +0400

    Обновление до версии 2.39.0

---
+++ SDK/Midway.ObjectModel/SimpleSignatureRegulationAcceptedValueSettingRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Midway.ObjectModel
+{
+    /// <summary>
+    /// Запрос на обновление настройки принятия регламента использования простой ЭП.
+    /// </summary>
+    [DataContract]
+    public class SimpleSignatureRegulationAcceptedValueSettingRequest
+    {
+        /// <summary>
+        /// ИД организации.
+        /// </summary>
+        [DataMember(IsRequired = true)]
+        public int OrganizationId { get; set; }
+
+        /// <summary>
+        /// Флаг принятия регламента использования простой ЭП.
+        /// </summary>
+        /// <c>true</c>, если принят регламент использования простой ЭП.
+        [DataMember(IsRequired = true)]
+        public bool Value { get; set; }
+    }
+}
